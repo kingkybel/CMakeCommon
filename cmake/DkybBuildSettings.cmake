4808792eafa2f0a8e34ff4a2c8e761249be87ce4
Dieter J Kybelksties <github@kybelksties.com>
2026-03-20 15:10:47 +0000

    Update for sub-modules added

--- cmake/DkybBuildSettings.cmake
+++ cmake/DkybBuildSettings.cmake
@@ -58,3 +58,21 @@ function(dkyb_apply_common_settings)
 
     message(STATUS "BUILD TYPE ${CMAKE_BUILD_TYPE}")
 endfunction()
+
+function(dkyb_update_submodules)
+    find_package(Git REQUIRED)
+
+    execute_process(
+        COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        RESULT_VARIABLE _result
+        OUTPUT_VARIABLE _output
+        ERROR_VARIABLE _error
+    )
+
+    if(NOT _result EQUAL 0)
+        message(FATAL_ERROR "Failed to update submodules: ${_error}")
+    else()
+        message(STATUS "Submodules updated successfully")
+    endif()
+endfunction()
